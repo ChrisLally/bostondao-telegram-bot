eff6428ee68e20463555e06b394098cf3443380b
Chris Lally <24978693+ChrisLally@users.noreply.github.com>
2023-12-09 21:44:55 -0500

    ready for cloud run I think?

--- bostondao-telegram-bot-function/main.py
+++ bostondao-telegram-bot-function/main.py
@@ -1,59 +1,57 @@
-# to update requirements.txt run: pipreqs --savepath requirements.txt .
+# import traceback
+# import asyncio
+# import requests
+# import json
 import os
-import traceback
-import asyncio
-import requests
-import json
-from dotenv import load_dotenv  # Import the load_dotenv function
+from dotenv import load_dotenv
+from telegram import KeyboardButton, ReplyKeyboardMarkup, Bot
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-local = False if os.getenv("PRODUCTION") else True
-# Load environment variables from the .env file only if running locally
+
+local = not bool(os.getenv("PRODUCTION"))
 if local:
-    load_dotenv()
+   load_dotenv()
 
-telegram_api_key=os.getenv("TELEGRAM_API_KEY")
-
-async def respondToMessage(input_json):
-    chatId=input_json["message"]["chat"]["id"]
-    text="the python function got your message! "+input_json["message"]["text"]+" AND here is all the data we got and can work with: "+json.dumps(input_json)
-    get_url=f"https://api.telegram.org/bot{telegram_api_key}/sendMessage?chat_id={chatId}&text={text}"
-    print('getting', get_url)
-    requests.get(get_url)
-    
-    return "ok"
-
-async def handle_request_async(request):
-    if local:
-        assert isinstance(request, dict)
-        request_json = request
-    else:
-        request_json = request.get_json(silent=True)
-        
-    print("got request!")
-    print(request_json)
-    
-    try:
-        if request_json.get("message"):
-            return_final = await respondToMessage(request_json)
-            return (return_final, 200)
-        else:
-            print('no message in payload')
-            return ('no message in payload', 500)
-
-    except Exception as e:
-        print(e)
-        traceback.print_exception(type(e), e, e.__traceback__)
-        return (str(e), 500)
-
-def handle_request(request):
-    return asyncio.run(handle_request_async(request))
-
-async def main():
-    example_request={'update_id': 7551784, 'message': {'message_id': 45, 'from': {'id': 755872907, 'is_bot': False, 'first_name': 'Chris', 'last_name': 'Lally | Fide.id', 'username': 'chrislally', 'language_code': 'en'}, 'chat': {'id': 755872907, 'first_name': 'Chris', 'last_name': 'Lally | Fide.id', 'username': 'chrislally', 'type': 'private'}, 'date': 1698847077, 'text': 'test2'}} 
-    result = await handle_request_async(example_request)
-    return result
+telegram_api_key = os.getenv("TELEGRAM_API_KEY")
+print('got key')
+print(telegram_api_key)
 
 
-if local:
-    result = asyncio.run(main())
-    print(result)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+   """Send a message when the command /start is issued."""
+   user = update.effective_user
+   await update.message.reply_html(
+       rf"Hi {user.mention_html()}!",
+       reply_markup=ForceReply(selective=True),
+   )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+   """Send a message when the command /help is issued."""
+   await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+   """Echo the user message."""
+   await update.message.reply_text(update.message.text)
+
+
+def initialize_bot():
+   # Initialize the bot, add handlers, and return the Application instance.
+   application = Application.builder().token(telegram_api_key).build()
+   application.add_handler(CommandHandler("start", start))
+   application.add_handler(CommandHandler("help", help_command))
+   application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+   return application
+
+def main(request=None): # CLOUD FUNCTION ENTRY POINT
+   # Create the bot instance and start polling.
+   application = initialize_bot()
+   #application.run_polling(allowed_updates=Update.ALL_TYPES)
+   application.run_polling(allowed_updates=Update.ALL_TYPES, webhook_url=f"http://0.0.0.0:{os.getenv('PORT')}")
+
+   return ("done", 200)
+
+if __name__ == "__main__": # LOCAL TESTING ONLY
+   main()
